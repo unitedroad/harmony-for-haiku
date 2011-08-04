34b830bebf1d288e11941cbc9264ce42ea53441b
Dhruwat Bhagat <unitedronaldo@yahoo.com>
2011-08-04 23:44:35 +0000

    Finish before trip

---
+++ classlib/makefiles.mk
@@ -0,0 +1,44 @@
+./modules/archive/src/main/native/archive/unix/makefile - x
+./modules/archive/src/main/native/zip/unix/makefile - x
+./modules/archive/src/main/native/zlib/unix/makefile - x
+./modules/auth/src/main/native/auth/unix/makefile
+./modules/awt/src/main/native/fontlib/unix/makefile
+./modules/awt/src/main/native/gl/unix/makefile
+./modules/awt/src/main/native/jpegdecoder/unix/makefile
+./modules/awt/src/main/native/lcmm/unix/makefile
+./modules/awt/src/main/native/linuxfont/unix/makefile
+./modules/awt/src/main/native/oglwrapper/unix/makefile
+./modules/awt/src/main/native/x11wrapper/unix/makefile
+./modules/imageio/src/main/native/jpegencoder/unix/makefile
+./modules/imageio/src/main/native/pngencoder/unix/makefile
+./modules/instrument/src/main/native/instrument/unix/makefile
+./modules/luni/src/main/native/fdlibm/unix/makefile - x
+./modules/luni/src/main/native/hyzip/unix/makefile - x
+./modules/luni/src/main/native/launcher/unix/makefile - x
+./modules/luni/src/main/native/luni/unix/makefile - x
+./modules/luni/src/main/native/vmi/unix/makefile - x
+./modules/luni/src/main/native/vmls/unix/makefile - x
+./modules/misc/src/main/native/accessors/unix/makefile
+./modules/nio/src/main/native/nio/unix/makefile
+./modules/nio_char/src/main/native/niochar/unix/makefile
+./modules/portlib/src/main/native/a2e/unix/makefile
+./modules/portlib/src/main/native/common/unix/makefile - x
+./modules/portlib/src/main/native/pool/unix/makefile - x
+./modules/portlib/src/main/native/port/unix/makefile - x
+./modules/portlib/src/main/native/thread/unix/makefile - x
+./modules/portlib/src/main/native/thrstub/unix/makefile - x
+./modules/portlib/src/test/native/hycpu/unix/makefile
+./modules/portlib/src/test/native/hyerror/unix/makefile
+./modules/portlib/src/test/native/hyfile/unix/makefile
+./modules/portlib/src/test/native/hyfiletext/unix/makefile
+./modules/portlib/src/test/native/hygp/unix/makefile
+./modules/portlib/src/test/native/hyipcmutex/unix/makefile
+./modules/portlib/src/test/native/hymem/unix/makefile
+./modules/portlib/src/test/native/hymmap/unix/makefile
+./modules/portlib/src/test/native/hynls/unix/makefile
+./modules/portlib/src/test/native/hyport/unix/makefile
+./modules/portlib/src/test/native/hystr/unix/makefile
+./modules/portlib/src/test/native/hysysinfo/unix/makefile
+./modules/portlib/src/test/native/hytime/unix/makefile
+./modules/portlib/src/test/native/init/unix/makefile
+./modules/security/src/main/native/security/unix/makefile
